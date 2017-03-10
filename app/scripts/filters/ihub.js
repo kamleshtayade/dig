3d8e3e5c1777aad4569c644b91aad5be27d36ccd
Kamlesh <kamleshtayade@gmail.com>
2017-03-10 18:46:26 +0530

    check for undefined
    fix typeerror

--- app/scripts/filters/ihub.js
+++ app/scripts/filters/ihub.js
@@ -18,8 +18,10 @@ angular.
   .filter('facetFilter',function(){    
     return function(input){
       var newArray = [];
-      for(var x=0;x<input.length;x+=2){
-        newArray.push(input[x]);
+      if(input!= undefined){
+        for(var x=0;x<input.length;x+=2){
+          newArray.push(input[x]);
+        }
       }
       return newArray;
     }
